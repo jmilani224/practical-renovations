df847cda5fd46f885fd00765007273552aca7f23
jmilani224 <jmilani224@gmail.com>
2020-11-29 23:45:44 -0500

    added avitar to blog template

--- src/components/about-jesse.js
+++ src/components/about-jesse.js
@@ -3,6 +3,7 @@ import { Flex, Avatar, Box } from '@chakra-ui/core'
 import { useStaticQuery, graphql } from 'gatsby'
 import { SideBarVerticalSpace, SidebarText } from '../templates/blog-post-template.js'
 import { RichText } from 'prismic-reactjs'
+import avi from '../images/avi.jpg'
 
 const AboutJesse = ({ textAlign }) => {
 
@@ -26,7 +27,7 @@ const AboutJesse = ({ textAlign }) => {
     w="100%"
     alignItems="center"
     >
-        <Avatar name="Jesse Carter" src="https://scontent-mia3-1.cdninstagram.com/v/t51.2885-19/s320x320/109026022_578692232814312_5935269997174878413_n.jpg?_nc_ht=scontent-mia3-1.cdninstagram.com&_nc_ohc=JtbO3LaiWEMAX86moSF&oh=93a3141164ca934381384fb10e5a74b2&oe=5F41A6F7" size="2xl" />
+        <Avatar name="Jesse Carter" src={avi} size="2xl" />
         <SideBarVerticalSpace>
             <SidebarText>
                 <Box
